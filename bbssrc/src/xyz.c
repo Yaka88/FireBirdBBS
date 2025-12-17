95738df4003d743e820348fa51414ee3f386684d
Yaka <yaka88@gmail.com>
2025-12-17 20:46:39 +0800

    update exec

--- bbssrc/src/xyz.c
+++ bbssrc/src/xyz.c
@@ -25,6 +25,8 @@ $Id: xyz.c,v 1.1 2000/01/15 01:45:30 edwardc Exp $
 
 #define EXTERN
 #include "bbs.h"
+#include <unistd.h>
+#include <sys/wait.h>
 
 int     use_define = 0;
 extern int iscolor;
@@ -619,9 +621,18 @@ if ((fd=open("etc/connlist",O_RDONLY))>=0)
 	  refresh();
 	  fflush(stdout);
 	  fflush(stderr);
+
+	
+	if (fork() == 0) {
+      setsid();          // 这句才是解决 SSH 密码提示出现在错屏幕的唯一钥匙
 	  dup2(0, 1); // 强制把 stdout 指向用户的 socket
-	  dup2(0, 2); // 强制把 stderr 指向用户的 socket
-	  system(my_commd);
+	  dup2(0, 2); // 强制把 stderr 指向用户的 socket 
+    // system(my_commd);
+      execl("/bin/sh", "sh", "-c",my_commd, (char *)0);
+      _exit(1);
+	} else {
+       wait(NULL);        // 父进程在这里安静地等，不干扰输入
+	}
 	  close(myhandle);
 	  clear();
 	}
